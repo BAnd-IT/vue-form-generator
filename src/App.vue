06601c3af815b5b7ff360d034ab8da5c02ebe908
Andrei Babkin <bandit.tcpc@gmail.com>
2025-12-27 20:37:11 +0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app">Initial</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Inter', sans-serif;
+}
+
+.app {
+  .test {
+    color: red;
+  }
+}
+</style>
